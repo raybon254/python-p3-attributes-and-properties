1c9089a7f09101606b05bf6e1cea98d63792a117
Brian <musyimib449@gmail.com>
2025-05-15 22:34:31 +0300

    implemntation of getters and setter and relation to protected words

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -11,5 +10,32 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
+    def __init__(self,name = 'ray',breed = 'Corgi'):
+        self.name = name
+        self.breed = breed
+        pass
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self,name):
+        if isinstance (name, str) and  1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self,breed):
+        if isinstance (breed, str) and breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
     pass
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,33 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self,name = 'Ray',job = 'ITC'):
+        self.name = name
+        self.job = job
+        pass
+
+    @property
+    def name(self):
+        return self._name.title()
+    
+    @name.setter
+    def name(self,name):
+        if isinstance (name,str) and 1 <= len(name) <= 25:
+            self._name = name
+        
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self,job):
+        if isinstance (job,str) and job in APPROVED_JOBS:
+            self._job = job
+        
+        else:
+            print( "Job must be in list of approved jobs.")
+
     pass
